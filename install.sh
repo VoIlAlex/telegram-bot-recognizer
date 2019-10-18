6faf4d2ad78a6672e28b365063275dbb9ef51d70
voilalex <ilya.vouk@gmail.com>
2019-10-18 15:43:40 +0300

    Fix weights URL

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 
 # Download weights of NN 
 cd data
-wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3-spp.cfg -O yolov3.cfg
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3-spp.cfg -O yolov3.cfg
 wget https://pjreddie.com/media/files/yolov3-spp.weights -O yolov3.weights
 cd ..
 
